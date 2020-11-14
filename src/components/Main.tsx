80f4e003c714c2dfae48b25a0f59d5e2c89fdf5e
Tom Quast <tomquast89@gmail.com>
2020-11-14 21:29:18 +0100

    Added modal
    Moved components into modal

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import Interests from './Interests';
-import Profile from './Profile';
-import Settings from './Settings';
 import styles from './Main.module.scss';
+import Options from './options/Options';
 
 export default function Main() {
   return (
     <div className={styles.main}>
-      <div className={styles.container}>
-        <Profile />
-        <Interests />
-        <Settings />
-      </div>
+      <Options className={styles.options} />
     </div>
   );
 }
---
+++ src/components/options/Options.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Interests from './Interests';
+import Profile from './Profile';
+import Settings from './Settings';
+import styles from './Options.module.scss';
+import Modal from 'react-modal';
+
+const modalStyles = {
+    content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)',
+        padding               : '0'
+    }
+};
+
+type Props = {
+    className?: string;
+}
+
+export default function Options({ className }: Props) {
+    const [modalIsOpen, setModalIsOpen] = React.useState(false);
+    return (
+        <>
+            <button className={className && className} onClick={() =>setModalIsOpen(true)}>⚙️</button>
+            <Modal
+                closeTimeoutMS={300}
+                isOpen={modalIsOpen}
+                onRequestClose={() =>setModalIsOpen(false)}
+                style={modalStyles}
+                contentLabel="Settings Modal">
+                <div className={styles.container}>
+                    <Profile />
+                    <Interests />
+                    <Settings />
+                </div>
+            </Modal>
+       </>
+    );
+}
